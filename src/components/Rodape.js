3a2cb6991cf27e43df9f05841e5b13be6bd71f46
Carlos Possa <carlos@mastersig.com.br>
2021-02-18 19:06:28 -0300

    Versão inicial

---
+++ src/components/Rodape.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <p>
+                Conteúdos cedidos pela Editora Novatec &#8212; &copy; Copyright 2020
+            </p>
+        </footer>
+    );
+}
+
+export default Rodape;
